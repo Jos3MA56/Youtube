ea9d53c4c59cb4a69c2c4468c9e61520f49cd65f
Jos3MA56 <20230087@uthh.edu.mx>
2025-02-25 23:14:56 -0600

    Update index.ts

--- my-app/index.ts
+++ my-app/index.ts
@@ -3,9 +3,11 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 import YouTubeHome from './screens/Youtube';
 import CalPropinas from './screens/CalPropinas';
+import clima from './screens/Clima';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(YouTubeHome);
 registerRootComponent(CalPropinas);
+registerRootComponent(clima);
